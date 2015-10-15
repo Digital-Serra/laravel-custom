e0763a34a634f3ca73a796c516c0f752617ab524
Mauri de Souza Nunes <mauri870@gmail.com>
2015-10-15 11:58:05 -0300

    Auth-master title updated

--- resources/views/layouts/auth-master.blade.php
+++ resources/views/layouts/auth-master.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
     @section('header')
     @show
-    <title>FuturaLuz::@yield('title')</title>
+    <title>Company::@yield('title')</title>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
