addbcd6a167409c32ba291ba669e317ae981c411
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-12 09:05:14 -0200

    Added new middleware for dashboard routes

--- app/Http/routes/dashboard.php
+++ app/Http/routes/dashboard.php
@@ -1,5 +1,5 @@
 <?php
 //DashBoard Routes
-Route::group(['middleware' => 'auth','namespace'=>'Dashboard'], function () {
+Route::group(['middleware' => ['auth', 'setUserVariable'],'namespace'=>'Dashboard'], function () {
     Route::get('/dashboard',['as'=>'dashboard.index','uses'=>'DashController@index']);
 });
