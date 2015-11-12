2749bc47a125dfb425a4472aefd1cd0b6ad7909e
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-12 09:03:58 -0200

    Created middleware for share the user variable

---
+++ app/Http/Middleware/SetUserVariable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
+
+class SetUserVariable
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check()){
+            View::share('user', Auth::user()->name);
+        }
+
+        return $next($request);
+    }
+}
