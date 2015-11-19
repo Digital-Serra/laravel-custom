c55aa8a4dcfcb076431897d10d063685c48f3f5c
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-19 13:49:44 -0200

    Create repository and interface repository

---
+++ app/Repositories/UserRepositoryEloquent.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mauri870
+ * Date: 19/11/15
+ * Time: 13:33
+ */
+
+namespace App\Repositories;
+
+
+use App\Models\User;
+use Prettus\Repository\Eloquent\BaseRepository;
+
+class UserRepositoryEloquent extends BaseRepository implements UserRepositoryInterface
+{
+    /**
+     * Specify the model class name
+     * @return mixed
+     */
+    public function model()
+    {
+        return User::class;
+    }
+}
---
+++ app/Repositories/UserRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mauri870
+ * Date: 19/11/15
+ * Time: 13:39
+ */
+
+namespace App\Repositories;
+
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+interface UserRepositoryInterface extends RepositoryInterface
+{
+
+}
