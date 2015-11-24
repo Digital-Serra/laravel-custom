6630d91ce509c595966ae5231e629f8fb62a5863
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-24 07:54:46 -0200

    Removed unused UserRepository

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
-use Validator;
+use App\Entities\User;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Repositories\UserRepositoryEloquent;
-use App\Repositories\UserRepositoryInterface;
-use Illuminate\Http\Request;
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
