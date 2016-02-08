0ed2c54a01325226dcbc863084478d95eac7c1b5
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-08 14:14:52 -0200

    Add method for insert test user

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,9 @@
 
 namespace App\Tests;
 
+use App\Entities\User;
 use App\Tests\Traits\DatabaseSeeds;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTest;
@@ -33,5 +35,21 @@ class TestCase extends BaseTest
         $app->make(Kernel::class)->bootstrap();
 
         return $app;
+    }
+
+    /**
+     * Create a new test user
+     *
+     * @param $name
+     * @param $email
+     * @param $password
+     * @return bool
+     */
+    public function insertTestUser($name, $email, $password){
+        if(count(User::all()) == 0){
+            User::create(['name'=>$name,'email'=>$email,'password'=>bcrypt($password),'created_at'=>Carbon::now(),'updated_at'=>Carbon::now()]);
+        }
+
+        return true;
     }
 }
