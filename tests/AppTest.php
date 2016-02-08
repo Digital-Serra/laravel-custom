fe223325c0ff05189c17c9e8e43d0842dc637e4f
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-08 13:45:01 -0200

    Renamed

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class AppTest extends TestCase
 {
     /**
      * A basic functional test example.
