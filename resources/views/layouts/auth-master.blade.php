c194a0291e56bc072335b69ccf79fb3f394ef5e4
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-11 16:04:23 -0200

    Update auth layout template

--- resources/views/layouts/auth-master.blade.php
+++ resources/views/layouts/auth-master.blade.php
@@ -1,81 +1,74 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
-    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{-- Link Bootstrap and Font-Awesome --}}
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-    {{-- Auth page style--}}
-    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
-    @section('header')
-    @show
-    <title>Company::@yield('title')</title>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="description" content="{{ env('APP_NAME') }} - Login"/>
+    <meta name="author" content="Mauri de Souza Nunes(github.com/mauri870) - Digital Serra Tecnologia Digital"/>
+    <link rel="icon" href="http://demo.neontheme.com/assets/images/favicon.ico">
+    <title>{{ $title }}</title>
+    @include('dashboard.includes.styles')
 </head>
-<body>
-<div class="container">
-    <div id="loginbox" class="mainbox col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3">
-
-        <div class="row text-center">
-            <img class="img-responsive center-block" src="{{ asset('img/logo.svg') }}"/>
+<body class="page-body login-page login-form-fall" data-url="{{ env('APP_URL') }}">
+<script type="text/javascript">
+    var baseurl = '{{ env('APP_URL') }}';
+</script>
+<div class="login-container">
+    <div class="login-header login-caret">
+        <div class="login-content">
+            <a href="{{ route('dashboard.index') }}" class="logo">
+                <img src="{{ asset('assets/dashboard/images/logo@2x.png') }}" width="120" alt=""/>
+            </a>
+            <!-- progress bar indicator -->
+            <div class="login-progressbar-indicator"><h3>43%</h3> <span>Efetuando o login...</span></div>
         </div>
-
-        <div class="row  text-center">
-            <div class="col-md-12">
-                @if (Session::has('status'))
-                    <div class="alert alert-warning">
-                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-
-                        {{ Session::get('status') }}
-                    </div>
-                @endif
-
-                @unless($errors->isEmpty())
-                    <ul style="list-style: none;">
-                        @foreach($errors->getMessages() as $error)
-                            <li>
-                                <p class="alert alert-danger">
-                                    <a href="#" class="close" data-dismiss="alert"
-                                       aria-label="close">&times;</a>
-                                    {{ $error[0] }}
-                                </p>
-                            </li>
-                        @endforeach
-                    </ul>
-                @endunless
-                @if (Session::has('flash_notification.message'))
-                    <div class="alert alert-{{ Session::get('flash_notification.level') }}">
-                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-
-                        {{ Session::get('flash_notification.message') }}
-                    </div>
-                @endif
-            </div>
-        </div>
-        @section('body')
-        @show
     </div>
-    <div id="particles-js"></div>
+    <div class="login-progressbar">
+        <div></div>
+    </div>
+    <br><br>
+    <div class="row  text-center">
+        <div class="col-sm-6 col-sm-offset-3 text-center">
+            @if (Session::has('status'))
+                <div class="alert alert-warning">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+
+                    {{ Session::get('status') }}
+                </div>
+            @endif
+
+            @unless($errors->isEmpty())
+                <ul style="list-style: none;">
+                    @foreach($errors->getMessages() as $error)
+                        <li>
+                            <p class="alert alert-danger">
+                                <a href="#" class="close" data-dismiss="alert"
+                                   aria-label="close">&times;</a>
+                                {{ $error[0] }}
+                            </p>
+                        </li>
+                    @endforeach
+                </ul>
+            @endunless
+            @if (Session::has('flash_notification.message'))
+                <div class="alert alert-{{ Session::get('flash_notification.level') }}">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+
+                    {{ Session::get('flash_notification.message') }}
+                </div>
+            @endif
+        </div>
+    </div>
+    @yield('body')
+
 </div>
-<div class="footer">
-    @section('footer')
-    @show
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/auth/particles.min.js') }}"></script>
-    <script>
-        /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-        particlesJS.load('particles-js', 'js/auth/particles.json', function () {
-            console.log('callback - particles.js config loaded');
-        });
-    </script>
-</div>
+@include('dashboard.includes.scripts')
+<!-- JavaScripts initializations and stuff -->
+<!-- Demo Settings -->
+<script src="{{ asset('assets/dashboard/js/neon-custom.js') }}" id="script-resource-17"></script>
+<script src="{{ asset('assets/dashboard/js/neon-demo.js') }}" id="script-resource-18"></script>
+<script src="{{ asset('assets/dashboard/js/neon-skins.js') }}" id="script-resource-19"></script>
+@yield('scripts')
 </body>
 </html>
