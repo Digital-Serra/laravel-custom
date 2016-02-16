1f6a7abe93098cf9dfe8122b662fe0d8d6de99bc
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-16 09:09:27 -0200

    Create model and migration for notifications

---
+++ app/Entities/Notification.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+class Notification extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'email', 'password'];
+}
