d67c43c3e0c3664d1097dee257a58cc13a475513
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-17 13:08:10 -0200

    Add routes to profile

--- app/Http/routes/dashboard.php
+++ app/Http/routes/dashboard.php
@@ -2,4 +2,10 @@
 //DashBoard Routes
 Route::group(['middleware' => ['auth', 'SetGlobalAuthVariables'],'namespace'=>'Dashboard', 'prefix'=>'dashboard'], function () {
     Route::get('/',['as'=>'dashboard.index','uses'=>'DashController@index']);
+
+    //Update profile
+    Route::group(['prefix' => 'profile'], function () {
+        Route::get('/edit',['as'=>'dashboard.profile.edit','uses'=>'ProfileController@edit']);
+        Route::put('/edit',['as'=>'dashboard.profile.update','uses'=>'ProfileController@update']);
+    });
 });
