d9243d600e2c8cac0e184163a9bdab4ee3d02742
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-18 12:43:47 -0200

    Update profile test

--- tests/Dashboard/ProfileTest.php
+++ tests/Dashboard/ProfileTest.php
@@ -2,20 +2,43 @@
 
 namespace App\Tests\Dashboard;
 
+use App\Entities\User;
 use App\Tests\TestCase;
 
 class ProfileTest extends TestCase
 {
     /**
-     * Test dashboard default route.
+     * Test profile edit route.
      *
      * @return void
      */
-    public function testDefaultRoute()
+    public function testProfileEditRoute()
     {
        $this->authenticate();
 
        $this->visit('/dashboard/profile/edit')
            ->see('Editar Perfil');
+    }
+
+    /**
+     * Test can update a profile.
+     *
+     * @return void
+     */
+    public function testCanUpdateProfile()
+    {
+       $this->authenticate();
+
+       $this->visit('/dashboard/profile/edit')
+           ->type('ThisIsATest','name')
+           ->type('this@isatest.com','email')
+           ->type('12345678','old_password')
+           ->type('87654321','password')
+           ->type('87654321','password_confirmation')
+           ->press('Atualizar perfil')
+           ->seePageIs('/dashboard/profile/edit');
+
+        $this->assertEquals('ThisIsATest', User::where('name','ThisIsATest')->first()->name);
+
     }
 }
