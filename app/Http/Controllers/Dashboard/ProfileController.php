67d752c4b9ed3c477d983a82c96ce5af644c0b20
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-18 10:05:11 -0200

    Update edition of profile

--- app/Http/Controllers/Dashboard/ProfileController.php
+++ app/Http/Controllers/Dashboard/ProfileController.php
@@ -2,14 +2,32 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Entities\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Requests\ProfileFormRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Krucas\Notification\Facades\Notification;
 
 class ProfileController extends Controller
 {
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     * ProfileController constructor.
+     * @param User $user
+     * @param Notification $notification
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+    
     /**
      * Display a listing of the resource.
      *
@@ -68,11 +86,28 @@ class ProfileController extends Controller
      * Update the specified resource in storage.
      *
      * @param ProfileFormRequest $request
-     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function update(ProfileFormRequest $request)
     {
-        //
+        // Verify if the passwords match...
+        if (Hash::check($request->old_password, auth()->user()->getAuthPassword()))
+        {
+            $this->user->find(auth()->user()->id)
+                ->update([
+                    'name'=>$request->name,
+                    'email'=>$request->email,
+                    'password' => bcrypt($request->password)
+                ]);
+
+            Notification::success('Seu Perfil foi atualizado com sucesso');
+
+            return redirect()->back();
+        }
+
+        Notification::error('Por favor informe a sua senha atual corretamente!');
+
+        return redirect()->back();
     }
 
     /**
