3695983ed98bdf0b2045b0338cf6b1e8957bd951
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-19 08:56:19 -0200

    Add trait for authenticate user

---
+++ tests/Traits/Authenticate.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author    Mauri de Souza Nunes <mauri870@gmail.com>
+ * @copyright Copyright (c) 2015, Mauri de Souza Nunes <github.com/mauri870>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
+namespace App\Tests\Traits;
+
+
+use App\Entities\User;
+use Illuminate\Support\Facades\Auth;
+
+trait Authenticate
+{
+    /**
+     * Authenticate a user
+     *
+     * @before
+     * @param string $email
+     * @param string $password
+     * @return bool
+     */
+    public function authenticate($email = 'test@test.com', $password = '12345678'){
+        Auth::attempt(['email' => $email, 'password' => $password]);
+
+        return true;
+    }
+}
