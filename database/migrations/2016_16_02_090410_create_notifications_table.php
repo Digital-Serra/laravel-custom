70051d3581f833b9145d5542bde2f036f4fc1917
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-22 10:02:54 -0300

    Add nullable to field title

--- database/migrations/2016_16_02_090410_create_notifications_table.php
+++ database/migrations/2016_16_02_090410_create_notifications_table.php
@@ -15,7 +15,7 @@ class CreateNotificationsTable extends Migration
         Schema::create('notifications', function (Blueprint $table) {
             $table->increments('id');
             $table->string('type');
-            $table->string('title');
+            $table->string('title')->nullable();
             $table->text('body');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
