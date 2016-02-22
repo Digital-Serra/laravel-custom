453a62611f8d96ea77244c785caefd6438cfa658
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-22 12:29:13 -0300

    Add notify facade

--- config/app.php
+++ config/app.php
@@ -230,6 +230,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Notify'    => \App\Notifications\Facades\Notify::class,
 
         //Collective
         'Form' => Collective\Html\FormFacade::class,
