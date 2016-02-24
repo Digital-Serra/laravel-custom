cd28d5269fd71b64c96bce042facac845cdde815
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-24 15:06:13 -0300

    Include form partial view

--- resources/views/dashboard/mail/create.blade.php
+++ resources/views/dashboard/mail/create.blade.php
@@ -24,30 +24,7 @@
             </div>
             <div class="mail-compose">
                 {!! Form::open(['route'=>'dashboard.mail.store','method'=>'post','id'=>'mail-form']) !!}
-                    <div class="form-group">
-                        {!! Form::label('to','Para:') !!}
-                        <input type="text" class="form-control" id="to" tabindex="1" placeholder="email1,email2,email3"/>
-                        <div class="field-options">
-                            <a href="javascript:;" onclick="$(this).hide(); $('#cc').parent().removeClass('hidden'); $('#cc').focus();" title="Cópia">CC</a>
-                            <a href="javascript:;"
-                               onclick="$(this).hide(); $('#bcc').parent().removeClass('hidden'); $('#bcc').focus();" title="Cópia Oculta">BCC</a>
-                        </div>
-                    </div>
-                    <div class="form-group hidden">
-                        {!! Form::label('cc','Cópia:') !!}
-                        <input type="text" class="form-control" placeholder="email1,email2,email3" id="cc" tabindex="2"/>
-                    </div>
-                    <div class="form-group hidden">
-                        {!! Form::label('bcc','Cópia Oculta:') !!}
-                        <input type="text" class="form-control" placeholder="email1,email2,email3" id="bcc" tabindex="2"/>
-                    </div>
-                    <div class="form-group">
-                        {!! Form::label('subject','Assunto:') !!}
-                        <input type="text" class="form-control" id="subject" tabindex="1"/>
-                    </div>
-                    <div class="compose-message-editor">
-                        {!! Form::textarea('message',null,['class'=>'form-control wysihtml5','data-stylesheet-url'=>asset('assets/dashboard/wysihtml5/wysihtml5-color.css'),'id'=>'sample_wysiwyg']) !!}
-                    </div>
+                    @include('dashboard.forms._mail')
                 {!! Form::close() !!}
             </div>
         </div>
