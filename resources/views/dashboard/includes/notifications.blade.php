4786218f70d816f20675e1522dc2a6753be52026
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-25 10:42:50 -0300

    Fix typo in javascript

--- resources/views/dashboard/includes/notifications.blade.php
+++ resources/views/dashboard/includes/notifications.blade.php
@@ -1,29 +1,27 @@
 <script type="text/javascript">
     jQuery(document).ready(function ($) {
-        jQuery(document).ready(function ($) {
-            // Toastr Notifications
-            var opts = {
-                "closeButton": true,
-                "debug": false,
-                "positionClass": "toast-top-right",
-                "onclick": null,
-                "showDuration": "300",
-                "hideDuration": "1000",
-                "timeOut": "5000",
-                "extendedTimeOut": "1000",
-                "showEasing": "swing",
-                "hideEasing": "linear",
-                "showMethod": "fadeIn",
-                "hideMethod": "fadeOut"
-            };
+        // Toastr Notifications
+        var opts = {
+            "closeButton": true,
+            "debug": false,
+            "positionClass": "toast-top-right",
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        };
 
-            {!! Notification::showAll() !!}
+        {!! Notification::showAll() !!}
 
-            @unless($errors->isEmpty())
-                @foreach($errors->getMessages() as $error)
-                    toastr.error("{{ $error[0] }}", "Erro", opts);
-            @endforeach
-            @endunless
-        }, 3000);
-})
+        @unless($errors->isEmpty())
+            @foreach($errors->getMessages() as $error)
+                toastr.error("{{ $error[0] }}", "Erro", opts);
+        @endforeach
+        @endunless
+    }, 3000);
 </script>
