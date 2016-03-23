449b2e1b098bfe27758937f959ff07a3b798e500
Mauri de Souza Nunes <mauri870@gmail.com>
2016-03-23 15:56:54 -0300

    Add method for return a view with namespace

--- app/Core/Http/Controllers/Controller.php
+++ app/Core/Http/Controllers/Controller.php
@@ -10,4 +10,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $view_namespace;
+
+    /**
+     * Return a view with namespace
+     *
+     * @param string $view
+     * @param array  $data
+     * @param array  $mergeData
+     *
+     * @return \Illuminate\View\View
+     */
+    protected function view($view = null, $data = [], $mergeData = [])
+    {
+        if (!empty($this->view_namespace) and !str_contains($view, '::')) {
+            $view = $this->view_namespace . $view;
+        }
+        return view($view, $data, $mergeData);
+    }
 }
