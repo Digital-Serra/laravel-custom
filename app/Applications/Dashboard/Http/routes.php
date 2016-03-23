ebec1d7d75c3580bf60953671c825bf146f5a7ec
Mauri de Souza Nunes <mauri870@gmail.com>
2016-03-23 15:13:34 -0300

    Remove unused namespace

--- app/Applications/Dashboard/Http/routes.php
+++ app/Applications/Dashboard/Http/routes.php
@@ -1,15 +1,18 @@
 <?php
 //DashBoard Routes
-Route::group(['middleware' => ['auth','shareDashVarsToView'],'namespace'=>'Dashboard', 'prefix'=>'dashboard'], function () {
-    Route::get('/',['as'=>'dashboard.index','uses'=>'DashController@index']);
+Route::group(
+    [
+        'middleware' => ['auth', 'shareDashVarsToView'],
+        'prefix' => 'dashboard'], function () {
+    Route::get('/', ['as' => 'dashboard.index', 'uses' => 'DashController@index']);
 
     //Update profile
     Route::group(['prefix' => 'profile'], function () {
-        Route::get('/edit',['as'=>'dashboard.profile.edit','uses'=>'ProfileController@edit']);
-        Route::put('/edit',['as'=>'dashboard.profile.update','uses'=>'ProfileController@update']);
+        Route::get('/edit', ['as' => 'dashboard.profile.edit', 'uses' => 'ProfileController@edit']);
+        Route::put('/edit', ['as' => 'dashboard.profile.update', 'uses' => 'ProfileController@update']);
     });
 
-    Route::resource('notifications','NotificationController',['only'=>['index']]);
+    Route::resource('notifications', 'NotificationController', ['only' => ['index']]);
 
-    Route::resource('mail','MailController',['only'=>['create','store']]);
+    Route::resource('mail', 'MailController', ['only' => ['create', 'store']]);
 });
