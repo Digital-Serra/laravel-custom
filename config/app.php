ccc20686bb48f98b783580f6fb3832d98a13aecf
Mauri de Souza Nunes <mauri870@gmail.com>
2016-03-23 15:02:52 -0300

    Fix facade

--- config/app.php
+++ config/app.php
@@ -236,7 +236,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Notify'    => \App\Notifications\Facades\Notify::class,
+        'Notify'    => \App\Applications\Dashboard\Notifications\Facades\Notify::class,
 
         //Collective
         'Form' => Collective\Html\FormFacade::class,
