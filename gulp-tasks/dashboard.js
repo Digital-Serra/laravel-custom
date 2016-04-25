5226c62a3cf878d7436a6f3fb11d680d7da16eea
Mauri de Souza Nunes <mauri870@gmail.com>
2016-04-25 14:03:14 -0300

    Reorganize elixir tasks Remove unused operations

--- gulp-tasks/dashboard.js
+++ gulp-tasks/dashboard.js
@@ -2,61 +2,30 @@ var elixir = require('laravel-elixir');
 var bowerDir = './vendor/bower_components/';
 var gulp = require('gulp');
 
-//Disable generate *.css.map on compile .less files
-elixir.config.sourcemaps = false;
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir(function (mix) {
 
-    // Copy fonts to public/fonts
     mix.copy([
-            bowerDir + 'bootstrap/fonts',
-            bowerDir + 'font-awesome/fonts'
-        ],
-        'public/fonts'
-    )
+        bowerDir + 'bootstrap/fonts',
+        bowerDir + 'font-awesome/fonts'
+    ], 'public/assets/dashboard/fonts')
 
-    mix.copy([
-            bowerDir + 'bootstrap/fonts',
-            bowerDir + 'font-awesome/fonts'
-        ],
-        'public/assets/dashboard/fonts'
-        )
+    //Copy Sweetalert assets to public
+    mix.copy(bowerDir + 'sweetalert/dist/sweetalert.css', 'public/css')
+    mix.copy(bowerDir + 'sweetalert/dist/sweetalert.min.js', 'public/js')
 
-        //Compiling font awesome and bootstrap into a single file
-        .less([
-                bowerDir + 'bootstrap/less/bootstrap.less',
-                bowerDir + 'font-awesome/less/font-awesome.less'],
-            'resources/assets/css/base.css'
-        )
+    //Copy Datetimepicker and momentum assets to public
+    mix.copy(bowerDir + 'eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css', 'public/css')
+    mix.copy(bowerDir + 'eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js', 'public/js')
+    mix.copy(bowerDir + 'moment/min/moment.min.js', 'public/js')
 
-        //Copy Sweetalert assets to public
-        .copy(bowerDir + 'sweetalert/dist/sweetalert.css', 'public/css')
-        .copy(bowerDir + 'sweetalert/dist/sweetalert.min.js', 'public/js')
+    //Copy jquery, bootstrap and Particles.js to js folder
+    mix.copy(bowerDir + 'jquery/dist/jquery.min.js', 'public/js')
+    mix.copy(bowerDir + 'bootstrap/dist/js/bootstrap.min.js', 'public/js')
+    mix.copy(bowerDir + 'particles.js/particles.min.js', 'public/js/auth/')
 
-        //Copy Datetimepicker and momentum assets to public
-        .copy(bowerDir + 'eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css', 'public/css')
-        .copy(bowerDir + 'eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js', 'public/js')
-        .copy(bowerDir + 'moment/min/moment.min.js', 'public/js')
+    //Dashboard assets
+    mix.copy('resources/assets/dashboard', 'public/assets/dashboard')
 
-        //Copy jquery, bootstrap and Particles.js to js folder
-        .copy(bowerDir + 'jquery/dist/jquery.min.js', 'public/js')
-        .copy(bowerDir + 'bootstrap/dist/js/bootstrap.min.js', 'public/js')
-        .copy(bowerDir + 'particles.js/particles.min.js', 'public/js/auth/')
-
-        //Dashboard assets
-        .copy('resources/assets/dashboard', 'public/assets/dashboard')
-
-        //Main Vue File
-        .browserify('../dashboard/js/main.js','public/assets/dashboard/js/main.js')
+    //Main Vue File
+    mix.browserify('../dashboard/js/main.js', 'public/assets/dashboard/js/main.js')
 })
