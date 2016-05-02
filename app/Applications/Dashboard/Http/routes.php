a188775b67a4a8e6e32195581c4e8c03d1a62877
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-02 09:03:21 -0300

    Indent file correctly

--- app/Applications/Dashboard/Http/routes.php
+++ app/Applications/Dashboard/Http/routes.php
@@ -3,16 +3,17 @@
 Route::group(
     [
         'middleware' => ['auth', 'shareDashVarsToView'],
-        'prefix' => 'dashboard'], function () {
-    Route::get('/', ['as' => 'dashboard.index', 'uses' => 'DashController@index']);
+        'prefix' => 'dashboard'],
+    function () {
+        Route::get('/', ['as' => 'dashboard.index', 'uses' => 'DashController@index']);
 
-    //Update profile
-    Route::group(['prefix' => 'profile'], function () {
-        Route::get('/edit', ['as' => 'dashboard.profile.edit', 'uses' => 'ProfileController@edit']);
-        Route::put('/edit', ['as' => 'dashboard.profile.update', 'uses' => 'ProfileController@update']);
-    });
+        //Update profile
+        Route::group(['prefix' => 'profile'], function () {
+            Route::get('/edit', ['as' => 'dashboard.profile.edit', 'uses' => 'ProfileController@edit']);
+            Route::put('/edit', ['as' => 'dashboard.profile.update', 'uses' => 'ProfileController@update']);
+        });
 
-    Route::resource('notifications', 'NotificationController', ['only' => ['index']]);
+        Route::resource('notifications', 'NotificationController', ['only' => ['index']]);
 
-    Route::resource('mail', 'MailController', ['only' => ['create', 'store']]);
+        Route::resource('mail', 'MailController', ['only' => ['create', 'store']]);
 });
