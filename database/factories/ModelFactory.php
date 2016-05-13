f4b4b8361ba09949c35b41f43c91847d35769fed
Mauri de Souza Nunes <mauri870@gmail.com>
2016-05-13 14:04:44 -0300

    Fix length for remember_token field

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,7 +16,7 @@ $factory->define(App\Domains\Users\User::class, function (Faker\Generator $faker
         'name' => $faker->name,
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
+        'remember_token' => str_random(100),
     ];
 });
 
